2061c06667142669807113f41006779913125a8f
Brian Koech <brnkirwa@gmail.com>
2016-03-02 17:56:53 +0300

    Add responsivity to the app

--- app/components/nav-bar/TitleHead.jsx
+++ app/components/nav-bar/TitleHead.jsx
@@ -5,7 +5,10 @@ const TitleHead = () => (
   <div>
     <div className="row">
       {/*<img style={{margin: '0 auto'}} src="http://vietdms.com/image/News/634874776807460444logoDMS.png" />*/}
-      <h1 className="col-md-12 mastheadtitle">DMS</h1>
+      <h1 className="col-xs-12
+                col-sm-8
+                col-md-6
+                col-lg-4 mastheadtitle">DMS</h1>
       <h1 className="col-md-12 masthead">Document Management System</h1><br />
     </div>
   </div>
